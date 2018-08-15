5a8417c122ab4917ccfa8aa128885e7e098afb34
EmilProkopov <emmanuilprokopov@gmail.com>
2018-08-15 18:26:54 +0300

    Some changes

--- src/app/components/projects.component.ts
+++ src/app/components/projects.component.ts
@@ -8,15 +8,28 @@ import {Projects} from '../dictionaries/projects';
     templateUrl: 'projects.component.html'
 })
 export class ProjectsComponent {
+
     private changeLang: Subscription;
+    public colcount: number = 2;
     public source: string;
+
     constructor(public ts: TranslatorService) {
         this.changeLang = ts.changeLang$.subscribe(data => {
             this.setSource(data.lang);
         });
         this.source = Projects[ts.currentLang];
     }
+
     public setSource (lang: string) {
         this.source = Projects[lang];
+    }
+
+    onResize(event) {
+        if(event.target.innerWidth < 860) {
+            this.colcount = 1;
+        }
+        else {
+            this.colcount = 2;
+        }
     }
 }
