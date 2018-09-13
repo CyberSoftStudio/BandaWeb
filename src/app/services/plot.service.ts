9021f52fa0e60040bf6ca255d0e67ccc6acdaff6
emilprokopov <emmanuilprokopov@gmail.com>
2018-09-13 14:57:40 +0300

    Pretty plots

--- src/app/services/plot.service.ts
+++ src/app/services/plot.service.ts
@@ -8,39 +8,44 @@ import { Observable } from 'rxjs';
 
 export class PlotService {
   public apiURL: string = 'http://104.248.29.132:8080/data';
-
   constructor() {
   }
 
-  public getObservable(currencyCode: string) {
-    const lastSecondsToCheck = 3600;
-    const dataGetter = function sequenceSubscriber(observer) {
+
+  public getObservable (currencyCode: string) {
+    let lastSecondsToCheck;
+    if (currencyCode == "eth" || currencyCode == "ltc") {
+      lastSecondsToCheck = 3600;
+    } else {
+      lastSecondsToCheck = 21600;
+    }
+    let dataGetter =  function sequenceSubscriber(observer) {
 
       function startEmmiting() {
 
         async function _getDayBlocks() {
-          const toTime = Math.round(new Date().getTime() / 1000);
+          let toTime = Math.round(new Date().getTime() / 1000);
           const response = axios.get(
             'http://104.248.29.132:8080/data' + '?currency=' + currencyCode + '&last=' + lastSecondsToCheck);
 
-          console.log('http://104.248.29.132:8080/data' + '?currency=' + currencyCode + '&last=' + lastSecondsToCheck);
+          //console.log('http://104.248.29.132:8080/data' + '?currency=' + currencyCode + '&last=' + lastSecondsToCheck);
           return response || 0;
         }
 
         _getDayBlocks().then(response => {
           let resp = [];
           resp = response['data'];
-          if (resp.length > 0) {
+          if(resp.length > 0) {
             observer.next(resp);
           }
         }).catch(err => console.dir(err));
       }
-
       startEmmiting();
       setInterval(startEmmiting, 5000);
-    };
+
+    }
 
     return new Observable(dataGetter);
   }
-}
 
+}
